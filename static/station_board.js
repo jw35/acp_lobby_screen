0ba6c68192b249e7a1522f2a6b2da139eea31dca
Jon Warbrick <jw35@cam.ac.uk>
2018-01-12 10:23:05 +0000

    Assorted structure an layout changes
    Sort out layout (both of the framework and the station_board widgit),
    add refresh logic, logos, etc.

--- static/station_board.js
+++ static/station_board.js
@@ -1,28 +1,32 @@
-/* Station Board Widgit for ACP Lobby Pannel */
+/* Station Board Widgit for ACP Lobby Panel */
 
-function Station_Board(container) {
+function StationBoard(container, params) {
 
     this.container = container;
-    console.log("Instantiated Ststion Board for " + container)
+    this.params = params;
+    console.log("Instantiated StationBoard", container, params);
 
-    this.init = function(params) {
-        console.log("Running init for for " + this.container)
-        this.params = params;
+    this.init = function() {
+        console.log("Running Station_board.init", this.container);
         this.do_load();
     }
 
-    this.reload = function() {
-        console.log("Running reload for for " + this.container)
+    /*this.reload = function() {
+        console.log("Running StationBoard.reload", this.container);
         this.do_load();
-    }
+    }*/
 
     this.do_load = function myself() {
-        console.log("Running do_load for for " + this.container)
+        var self = this;
+        console.log("Running StationBoard.do_load", this.container);
         var url = "/station_board?station=" + this.params.station +
           "&offset=" + this.params.offset + " .station_board";
-        console.log("URI is " + url)
-        $(container).load(url, function() { console.log( "Load was performed." ); setTimeout(myself,10000); });
-        console.log("Done load " + url)
+        console.log("StationBoard.do_load URI", url);
+        console.log("Container", '#' + this.container)
+        $('#' + this.container).load(url, function() {
+            setTimeout(function() { self.do_load() }, 60000);
+        });
+        console.log("StationBoard.do_load done", this.container);
     }
 
 }
