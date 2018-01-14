936aa8b3726b9a319e2a654ee6c84041937911a3
Jon Warbrick <jw35@cam.ac.uk>
2018-01-14 19:40:48 +0000

    Added message_area widgit

---
+++ static/message_area/message_area.js
@@ -0,0 +1,26 @@
+/* Message Area Widgit for ACP Lobby Panel */
+
+function MessageArea(container, params) {
+
+    this.container = container;
+    this.params = params;
+    console.log("Instantiated MessageArea", container, params);
+
+    this.init = function() {
+        console.log("Running MesageArea.init", this.container);
+        this.do_load();
+    }
+
+    /*this.reload = function() {
+        console.log("Running StationBoard.reload", this.container);
+        this.do_load();
+    }*/
+
+    this.do_load = function myself() {
+        var self = this;
+        console.log("Running MessageArea.do_load", this.container);
+        $('#' + this.container).html('<div class="message_area">' + params.message + '</div>');
+        console.log("StationBoard.do_load done", this.container);
+    }
+
+}
