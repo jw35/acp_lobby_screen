41974a2203ac80152a36dc62b6025b17b4b00863
Jon Warbrick <jw35@cam.ac.uk>
2018-01-15 12:14:48 +0000

    Finish style tweaking for iframe_area
    This mainly involved turning off the default border around the iframe
    and suppressing any scrollbars.
    
    The latter is tricky. The iframe scrolling=no attribute was removed
    in HTML 5. The suggested replacement (the CSS 'overflow: hidden' CSS
    attribute) only works when applied to the source page, not the enclosing
    iframe ad typically you don't have control of the source page. This
    patch pragmatically applies scrolling=no to the iframe.

--- static/iframe_area/iframe_area.js
+++ static/iframe_area/iframe_area.js
@@ -11,18 +11,19 @@ function IframeArea(container, params) {
         this.do_load();
     }
 
-    /*this.reload = function() {
+    this.reload = function() {
         console.log("Running StationBoard.reload", this.container);
-        this.do_load();
-    }*/
+        $("#currentElement").attr("src", this.params.url);
+    }
 
     this.do_load = function myself() {
         var self = this;
         console.log("Running IframeArea.do_load", this.container);
         var frame = $('<iframe>')
           .attr('src',this.params.url)
-          .attr('height', '100%')
-          .attr('width', '100%');
+          // 'scrolling=no' is deprecated but I can't find a cosponsoring CSS attribute
+          .attr('scrolling', 'no')
+          .addClass('iframe_area');
         console.log("Frame: ", frame);
         $('#' + this.container).empty().append(frame);
         console.log("StationBoard.do_load done", this.container);
