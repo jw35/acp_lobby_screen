90af3ad59cb5eda4e544740af85cd18ebd4741fd
Jon Warbrick <jw35@cam.ac.uk>
2018-01-16 16:07:51 +0000

    Tidies widget .js files for jslint compatibility

--- static/traffic_map/traffic_map.js
+++ static/traffic_map/traffic_map.js
@@ -1,34 +1,38 @@
 /* Traffic Map Widget for ACP Lobby Screen */
 
+/*global $, google, document */
+
 function TrafficMap(container, params) {
+
+    'use strict';
 
     this.container = container;
     this.params = params;
     console.log("Instantiated TrafficMap", container, params);
 
-    this.init = function() {
-        console.log("Running TrafficMap.init", this.container);
+    this.init = function () {
+        console.log("Running init", this.container);
         this.do_load();
-    }
+    };
 
     /*this.reload = function() {
         console.log("Running StationBoard.reload", this.container);
         this.do_load();
     }*/
 
-    this.do_load = function myself() {
-        var self = this;
-        console.log("Running TrafficMap.do_load", this.container);
-        var map = new google.maps.Map(document.getElementById(this.container), {
-          zoom: this.params.zoom,
-          center: {lat: this.params.lat, lng: this.params.lng},
-          disableDefaultUI: true
+    this.do_load = function () {
+        console.log("Running do_load", this.container);
+        var map, trafficLayer;
+        map = new google.maps.Map(document.getElementById(this.container), {
+            zoom: this.params.zoom,
+            center: {lat: this.params.lat, lng: this.params.lng},
+            disableDefaultUI: true
         });
-        var trafficLayer = new google.maps.TrafficLayer({
+        trafficLayer = new google.maps.TrafficLayer({
             autoRefresh: true
         });
         trafficLayer.setMap(map);
         console.log("TragfficMap.do_load done", this.container);
-    }
+    };
 
 }
